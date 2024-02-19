2decd46732ff46bd6697252bea4fb681f9494636
brunasenracorrea <159476796+brunasenracorrea@users.noreply.github.com>
2024-02-19 10:02:39 -0300

    perguntas historia

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
 const perguntas = [
   {
-    pergunta: "Quando League of Legends foi oficialmente lançado?",
+    pergunta: "Qual é o nome do guerreiro demônio conhecido por sua sede insaciável por batalha em League of Legends?",
     respostas: [
-      "2008",
-      "2009",
-      "2010",
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Qual personagem é conhecido como o mascote de League of Legends?",
-    respostas: [
-      "Ashe",
-      "Teemo",
       "Darius",
+      "Aatrox",
+      "Yasuo",
     ],
     correta: 1
   },
   {
-    pergunta: "Qual é o nome do continente onde a maioria das batalhas em League of Legends ocorrem?",
-    respostas: [
-      "Runeterra",
-      "Shurima",
-      "Noxus",
-    ],
-    correta: 0
-  },
-  {
-    pergunta: "Quem foi o primeiro campeão a ser lançado em League of Legends?",
-    respostas: [
-      "Ashe",
-      "Ryze",
-      "Annie",
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Quantas linhas existem em Summoner's Rift, o mapa principal de League of Legends?",
-    respostas: [
-      "2",
-      "3",
-      "4",
-    ],
-    correta: 1
-  },
-  {
-    pergunta: "Qual é o título dado ao jogador que é reconhecido como o melhor do mundo em League of Legends?",
+    pergunta: "Qual é o título dado ao líder supremo de Noxus em League of Legends?",
     respostas: [
       "Rei",
-      "Campeão Mundial",
-      "Invocador",
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Qual foi o primeiro evento mundial de League of Legends?",
-    respostas: [
-      "Worlds",
-      "All-Star",
-      "Mid-Season Invitational",
-    ],
-    correta: 0
-  },
-  {
-    pergunta: "Qual é o nome da organização que supervisiona a produção e o desenvolvimento de League of Legends?",
-    respostas: [
-      "Blizzard",
-      "Riot Games",
-      "Valve Corporation",
+      "Imperador",
+      "Grão-General",
     ],
     correta: 1
   },
   {
-    pergunta: "Qual foi o primeiro campeão a ser lançado com uma skin lendária?",
+    pergunta: "Qual é o nome do vasto império desértico onde muitos campeões de League of Legends têm origem?",
     respostas: [
-      "Ashe",
-      "Annie",
-      "Tryndamere",
+      "Demacia",
+      "Piltover",
+      "Shurima",
     ],
     correta: 2
   },
   {
-    pergunta: "Quem é considerado o criador de League of Legends?",
+    pergunta: "Qual é o título dado ao líder do clã Ioniano em League of Legends?",
     respostas: [
-      "Marc Merrill",
-      "Steve Jobs",
-      "Bill Gates",
+      "Grão-Mestre",
+      "Ancião",
+      "Elder",
     ],
     correta: 0
+  },
+  {
+    pergunta: "Quem é o poderoso mago controlador de gelo em League of Legends?",
+    respostas: [
+      "Lissandra",
+      "Anivia",
+      "Brand",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é o título dado ao líder da tribo Freljord em League of Legends?",
+    respostas: [
+      "Rei",
+      "Grande Chefe",
+      "Reverenciado",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é o nome da cidade-estado tecnologicamente avançada onde a maioria dos campeões de Piltover reside em League of Legends?",
+    respostas: [
+      "Zaun",
+      "Bandle City",
+      "The Glade",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é o nome da temida guilda de assassinos de Zaun em League of Legends?",
+    respostas: [
+      "Ninja Tabi",
+      "Kinkou",
+      "Black Rose",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é o título dado ao líder da nação de Ionia em League of Legends?",
+    respostas: [
+      "Grão-Mestre",
+      "Ancião",
+      "Elder",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Quem é o famoso atirador conhecido por suas flechas congelantes em League of Legends?",
+    respostas: [
+      "Varus",
+      "Ashe",
+      "Lucian",
+    ],
+    correta: 1
   }
 ];
   
